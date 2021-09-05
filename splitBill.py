a730913be9b45188615ce41547a33cadf78f51a5
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-06 01:52:34 +0200

    added comments to the code

--- splitBill.py
+++ splitBill.py
@@ -1,11 +1,12 @@
 # program will calculate how a bill(amount) can be slit
-bill_total = float(input("Total bill amount: R"))
-split = int(input("How many people are splitting the bill? "))
-tip_percentage = float(input("How much would you like to tip? 5%, 10% or 15% ? "))
 
-tip_percentage /= 100
-tip_amount = bill_total * tip_percentage
-total = bill_total + tip_amount
-bill_split = total / split
+bill_total = float(input("Total bill amount: R"))   # total of the bill
+split = int(input("How many people are splitting the bill? "))  # how many way splitting it
+tip_percentage = float(input("How much would you like to tip? 5%, 10% or 15% ? "))  # tip
+
+tip_percentage /= 100   # convert to decimal
+tip_amount = bill_total * tip_percentage    # get the new total with tip included
+total = bill_total + tip_amount # add tip to the bill total
+bill_split = total / split  # split the bill
 
 print("-------   ------\nBill total (incl. tip): R",total,"\nEach person pays: R",bill_split)

db1781d50e0d166fbe3bb1f08970541e3f073383
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-06 02:04:52 +0200

    added coding exercise

---
+++ codingEx.py
@@ -0,0 +1,11 @@
+# interactive coding exercise
+# user enters 2 digit number, the program output the sum of the two digits
+
+number = input("Please enter a 2 digit number: ")
+
+digit1 = int(number[0])
+digit2 = int(number[1])
+
+sum = digit1 + digit2
+
+print("The sum is: ", sum)
